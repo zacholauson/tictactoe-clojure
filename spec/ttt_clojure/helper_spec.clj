176463030adbfae5c9f1495c1e9569a4f4fa988b
Zach Olauson <zmolauson@gmail.com>
2014-02-07 10:24:08 -0600

    cleanup possible moves test

--- spec/ttt_clojure/helper_spec.clj
+++ spec/ttt_clojure/helper_spec.clj
@@ -48,6 +48,5 @@
 
 (describe "possible-moves"
   (it "should return a list of indexes for the possible moves"
-    (should= [0 1 2 3 4 5 6 7 8] (possible-moves {:movelist [] :turn "x" :board ["-" "-" "-" "-" "-" "-" "-" "-" "-"]})))
-  (it "should return a list of indexes for the possible moves"
+    (should= [0 1 2 3 4 5 6 7 8] (possible-moves {:movelist [] :turn "x" :board ["-" "-" "-" "-" "-" "-" "-" "-" "-"]}))
     (should= [4 5 7 8] (possible-moves {:movelist [] :turn "x" :board ["o" "o" "o" "x" "-" "-" "x" "-" "-"]}))))
