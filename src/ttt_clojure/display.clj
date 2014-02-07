a306e6355d74af4cd50a446efd55722232b07034
Zach Olauson <zmolauson@gmail.com>
2014-02-07 09:59:05 -0600

    update display to accept error messages when asking for users next move

--- src/ttt_clojure/display.clj
+++ src/ttt_clojure/display.clj
@@ -9,7 +9,12 @@
 (defn valid-move? [gamestate move]
   (= "-" (get (:board gamestate) move)))
 
-(defn ask-human-for-move [gamestate]
-  (prn "Next Move: ")
-  (let [user-move (read-line)]
-    (if (valid-move? gamestate (parse-int user-move)) (parse-int user-move) (ask-human-for-move gamestate))))
+(defn ask-human-for-move
+  ([gamestate]
+    (prn "Next Move: ")
+    (let [user-move (read-line)]
+      (if (valid-move? gamestate (parse-int user-move)) (parse-int user-move) (ask-human-for-move gamestate "Invalid Move, Try Again "))))
+  ([gamestate error-message]
+    (prn gamestate) (prn (str error-message))
+    (let [user-move (read-line)]
+      (if (valid-move? gamestate (parse-int user-move)) (parse-int user-move) (ask-human-for-move gamestate "Invalid Move, Try Again ")))))
