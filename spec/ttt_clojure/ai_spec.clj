ac804a7343cbb51a68a581f4ddfe57ad1d6bd1a6
Zach Olauson <zmolauson@gmail.com>
2014-02-11 17:17:14 -0600

    remove bad minimax test and clean up a bad spec description

--- spec/ttt_clojure/ai_spec.clj
+++ spec/ttt_clojure/ai_spec.clj
@@ -21,12 +21,10 @@
     (should=  0 (leaf-score {:movelist [] :board [:x :o :o :o :x :x :x :x :o]}))))
 
 (describe "minimax"
-  (it "should return the minimax score for the given position"
+  (it "should return 0 if its the first move"
     (should= 0 (find-move {:movelist []        :board [:- :- :- :- :- :- :- :- :-]})))
   (it "should return take the win if its available"
     (should= 0 (find-move {:movelist [1 2 3 4] :board [:- :x :x :o :o :x :- :- :o]})))
-  (it "should return 0 if thats best move"
-    (should= 3 (find-move {:movelist [0 1]     :board [:x :o :- :- :- :- :- :- :-]})))
   (it "should block if a position is available that could allow o to win"
     (should= 1 (find-move {:movelist [0 2 3]   :board [:o :- :o :x :- :- :x :- :-]})))
   (it "when playing out every possible game they should all return true for win or tie"
