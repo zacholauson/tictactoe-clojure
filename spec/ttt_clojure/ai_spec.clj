e67c30d167155202533ea2b14b5d8357d38f13b5
Zach Olauson <zmolauson@gmail.com>
2014-02-13 15:50:40 -0600

    implement alpha beta pruning

--- spec/ttt_clojure/ai_spec.clj
+++ spec/ttt_clojure/ai_spec.clj
@@ -9,23 +9,25 @@
                                     (map (fn [possible-move]
                                              (let [newer-gamestate (move new-gamestate possible-move)]
                                                   (cond
-                                                    (win? newer-gamestate :o) false
+                                                    (win? newer-gamestate :o) [newer-gamestate false]
                                                     (win? newer-gamestate :x) true
                                                     :else (playout-every-game newer-gamestate))))
                                          (possible-moves new-gamestate))))))
 
 (describe "leaf-score"
   (it "should return the leaf score for the given gamestate"
-    (should=  1 (leaf-score {:movelist [] :board [:x :x :x :o :- :- :o :- :-]}))
-    (should= -1 (leaf-score {:movelist [] :board [:o :o :o :x :- :- :x :- :-]}))
-    (should=  0 (leaf-score {:movelist [] :board [:x :o :o :o :x :x :x :x :o]}))))
+    (should=  10 (leaf-score {:movelist [] :board [:x :x :x :o :- :- :o :- :-]} 0))
+    (should= -10 (leaf-score {:movelist [] :board [:o :o :o :x :- :- :x :- :-]} 0))
+    (should=   0 (leaf-score {:movelist [] :board [:x :o :o :o :x :x :x :x :o]} 0))))
 
 (describe "minimax"
   (it "should return 0 if its the first move"
     (should= 0 (find-move {:movelist []        :board [:- :- :- :- :- :- :- :- :-]})))
   (it "should return take the win if its available"
     (should= 0 (find-move {:movelist [1 2 3 4] :board [:- :x :x :o :o :x :- :- :o]})))
-  (it "should block if a position is available that could allow o to win"
+  (it "should block :o from winning if theres a chance"
     (should= 1 (find-move {:movelist [0 2 3]   :board [:o :- :o :x :- :- :x :- :-]})))
+  (it "should block if a position is available that could allow o to win"
+    (should= 6 (find-move {:movelist [0 4 1 2] :board [:x :x :o :- :o :- :- :- :-]})))
   (it "when playing out every possible game they should all return true for win or tie"
-    (should= true (every? true? (remove nil? (flatten (playout-every-game {:movelist [] :board [:- :- :- :- :- :- :- :- :-]})))))))
+    (should= true (every? true? (distinct (flatten (playout-every-game {:movelist [] :board [:- :- :- :- :- :- :- :- :-]})))))))
--- src/ttt_clojure/ai.clj
+++ src/ttt_clojure/ai.clj
@@ -3,45 +3,29 @@
 
 (declare minimax)
 
-(defn leaf-score [gamestate]
+(defn leaf-score [gamestate depth]
   (cond
-    (win? gamestate :x)  1
-    (win? gamestate :o) -1
-    (tied? gamestate) 0))
+    (win? gamestate :x) (+ 10 depth)
+    (win? gamestate :o) (+ -10 depth)
+    :else 0))
 
-(defn max-score [scores]
-  (apply max (flatten scores)))
+(defn minimax [gamestate depth alpha beta]
+  (if (or (game-over? gamestate) (= depth 5)) (leaf-score gamestate depth)
+    (if (even? depth)
+      (apply max (cons alpha
+        (filter #(< % beta)
+          (map (fn [possible-move]
+                 (minimax (move gamestate possible-move) (inc depth) alpha beta))
+               (possible-moves gamestate)))))
 
-(defn min-score [scores]
-  (apply min (flatten scores)))
-
-(defn playout-gamestates [gamestate]
-  (map #(minimax gamestate %) (possible-moves gamestate)))
-
-(defn return-score-for-gamestate [gamestate gamestate-scores]
-  (if (xs-turn? gamestate)
-      (max-score gamestate-scores)
-      (min-score gamestate-scores)))
-
-(defn score-gamestates [gamestate]
-  (return-score-for-gamestate gamestate (playout-gamestates gamestate)))
-
-(defn minimax [gamestate index]
-  (let [gamestate (move gamestate index)]
-    (if (game-over? gamestate) (leaf-score gamestate) (score-gamestates gamestate))))
-
-(defn map-scores-to-moves [gamestate]
-  (into {} (for [possible-move (possible-moves gamestate)]
-                [possible-move (minimax gamestate possible-move)])))
-
-(defn max-score? [score scores]
-  (= score (apply max (vals scores))))
-
-(defn pluck-best-score [scores]
-  (first (last (select-keys scores
-                            (for [[index score] scores :when (max-score? score scores)]
-                                   index)))))
+      (apply min (cons beta
+        (filter #(< % beta)
+          (map (fn [possible-move]
+                 (minimax (move gamestate possible-move) (inc depth) alpha beta))
+               (possible-moves gamestate))))))))
 
 (defn find-move [gamestate]
   (if (first-move? gamestate) 0
-    (pluck-best-score (map-scores-to-moves gamestate))))
+    (let [scores (into {} (for [possible-move (possible-moves gamestate)]
+                  [possible-move (minimax (move gamestate possible-move) 1 -100 100)]))]
+         (first (last (select-keys scores (for [[index score] scores :when (= score (apply max (vals scores)))] index)))))))
