374dd35170863bb1b37262d3825ff8fb507b856b
Zach Olauson <zmolauson@gmail.com>
2014-02-25 16:12:48 -0600

    update project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ttt-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "unbeatable tic tac toe"
+  :url "https://github.com/zacholauson/tictactoe-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -8,5 +8,4 @@
   :profiles {:dev {:dependencies [[speclj "2.9.1"]]}}
   :plugins [[speclj "2.9.1"]]
   :test-paths ["spec"]
-  :main ttt-clojure.core
-  :jvm-opts ["-Xmx5g" "-server"])
+  :main ttt-clojure.core)
