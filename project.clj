2de9ab9fa06a3d07beea39a714383198a8b737b1
Zach Olauson <zmolauson@gmail.com>
2014-02-28 09:58:51 -0600

    bump version number and require json package

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject ttt-clojure "0.1.0"
+(defproject ttt-clojure "0.1.1"
   :description "unbeatable tic tac toe"
   :url "https://github.com/zacholauson/tictactoe-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/data.json "0.2.4"]]
   :profiles {:dev {:dependencies [[speclj "2.9.1"]]}}
   :plugins [[speclj "2.9.1"]]
   :test-paths ["spec"]
