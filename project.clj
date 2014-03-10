3a71e0f0b30f14da341034580b888662b5fce403
Zach Olauson <zmolauson@gmail.com>
2014-03-10 14:54:28 -0500

    bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ttt-clojure "0.1.1-SNAPSHOT"
+(defproject ttt-clojure "0.1.2-SNAPSHOT"
   :description "unbeatable tic tac toe"
   :url "https://github.com/zacholauson/tictactoe-clojure"
   :license {:name "Eclipse Public License"
